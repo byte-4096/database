dd42881c862261122e2368b58ed488be99341ad4
epic9828 <fluffykittenbazooka@gmail.com>
2022-05-31 11:22:29 -0700

    re-coding for c++, java, and other launguage call support

--- database.c
+++ database.c
@@ -2,17 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
-
-/*
-
-This program is a cli for your own database! 
-no file encoding supported yet
-
-*/
-
-//global variables
-#define MAX 500
+#define MAX 5000
 
 char word = '!';
 char str[MAX];
@@ -23,47 +13,3 @@ FILE *fs1;
 char data[999];
 char ch[MAX];
 
-
-int main(){
-  char datas[500];
-  fs1 = fopen("database.dat", "r");
-  printf("PLEASE WAIT FOR DATA TO ARRIVE\n");
-  sleep(2);
-  printf("DATA:\n");
-  fgets(data, MAX, fs1);
-  fgets(ch,MAX,fs1);
-  printf("%s ", data);
-  printf("%s", ch);
-  sleep(3);
-  printf("\n\n\n\n\n");
-  
-  
-  fclose(fs1);
-  
-  fs1 = fopen("database.dat", "w");
-  fprintf(fs1,data);
-  fprintf(fs1,ch);
-
-  printf("DATABASE\n");
-
-  while(1){
-    printf("enter what to put into database, type ! to be finished:");
-    fgets(str,MAX,stdin);
-    str[counter];
-    if(str[counter] == word){
-      printf("ending...");
-      break;
-    }
-    fprintf(fs1,"%s",":");
-    fprintf(fs1, "%s", str);
-    printf("\nadding to output file\n");
-  }
-  printf("writing data to file");
-  for(int i = 10; i > 10; i++){
-      printf("adding... percentage:");
-      printf("#");
-  }
-  printf("Program shutting down...");
-  sleep(2);
-  fclose(fs1);
-}
