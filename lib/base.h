c2c21942571fdf591c162089199a464536c23bb7
epic9828 <fluffykittenbazooka@gmail.com>
2022-06-02 10:01:27 -0700

    encryption anddecryption protocalls

--- lib/base.h
+++ lib/base.h
@@ -3,41 +3,65 @@
 #include <unistd.h>
 
 
-
+//global constant values
 #define MAX 5000
 
+//strings, so many of them...
 char word = '!';
+char shift[];
 char str[MAX];
 char data[999];
 char ch[MAX];
+char *type[max];
+char name;
+char fname[max];
 
+//numbers
 int counter = 0;
-int done = 1;
+int done = 0;
 int xss = 1;
 
+//dectects encryption, so decryption can de-crypt it
+char detect(char namer){
 
-
-
-//creates a file with a specific typeset
-char createfs(const char *fname, const char *type, FILE *fs1){
-  //TODO: code 
-  fs1 = fopen(fname, type);
-  return NULL;
-}
-//puts data into a file
-char putfs(FILE fs, char data[]){
-  //TODO: code
-  
-  
-} 
-//gets data from a file
-char getfs(FILE fs, char keywords, char position){
-  //TODO: code
-   fscanf(fs, "%s", buff);
 }
 
-//encodes a specific file to binary
-char binfs(char fname){
-  //TODO: code  
 
+char encode(char data){
+  
+}
+
+//decodes ceaser cipher, 
+char cs1(char data){
+
+}
+
+//open a file for writing and reading, get data from it, and more
+int fsys(void){
+  //TODO: code
+  printf("Open file type\n");
+  sscanf(type, "%s");
+  printf("file name\n");
+  sscanf(fname, "%s");
+  while(done == 0){
+
+   
+    if ((fs1 = fopen(fname, type)) == NULL){
+        printf("file not found, creating file\n"); 
+        fs1 = fopen(fname, "a+");
+    }
+    else{
+      printf("File was located and opened successfuly");
+      break;
+    }
+    printf("ALGORITHM DECODER\n");
+    printf("are there any encoding in the file(y/n)");
+    sscanf(name, "%c");
+    if(name == "y"){
+
+    }
+    else{
+      printf("algorithms ")
+    }
+  }
 }
