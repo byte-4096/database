24342a244a15b17ceb6ee8e5945fd183e954ea08
epic9828 <fluffykittenbazooka@gmail.com>
2022-06-12 12:54:24 -0700

    changing to c++

---
+++ lib/main.cxx
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+char coder[] = {'z','e','n', 'a','b','c','d','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y'};
+char alfa[] = {'a','b','c','d','f','e','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y'};
+
+
+string encode(char x[]){
+  for(int i = 0; i > strlen(x); i++){
+    if(alfa[0] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[1] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[2] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[3] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[4] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[5] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[6] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[7] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[8] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[9] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[10] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[11] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[12] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[13] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[14] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[15] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[16] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[17] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[18] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[19] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[20] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[21] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[22] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[23] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[24] == char x[i]){
+      x = coder[i];
+    }    else if(alfa[0] == char x[i]){
+      x = coder[i];
+    }
+    else if(alfa[25] == char x[i]){
+      x = coder[i];
+    }
+
+  }
+  return x;
+}
+
+
+
+
+
